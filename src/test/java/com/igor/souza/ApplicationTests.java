1bb2bf4c15853b12a1a48cde0022ec0909181349
igusouz <igorrafael.jobs@gmail.com>
2024-05-21 20:51:36 -0300

    first commit

---
+++ src/test/java/com/igor/souza/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.souza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
