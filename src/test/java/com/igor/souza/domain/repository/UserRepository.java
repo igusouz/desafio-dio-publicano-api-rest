123e2d1dc4058d707af25847acab5fc535974863
igusouz <igorrafael.jobs@gmail.com>
2024-05-22 22:04:22 -0300

    Implementação da camada de domínio

---
+++ src/test/java/com/igor/souza/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.igor.souza.domain.repository;
+
+import com.igor.souza.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
